9db11855c7ff4a833b9e9923d5cf132d0f612ee5
Somjit Nag <somjitnag192@gmail.com>
2024-12-06 11:39:41 +0530

    virtual threads work, but no tracing context

--- tester/src/main/java/example/calc/tester/TestService.java
+++ tester/src/main/java/example/calc/tester/TestService.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestClient;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.ExecutorService;
 
 @Slf4j
 @Service
@@ -18,35 +19,41 @@ public class TestService {
     private EvaluateServiceGrpc.EvaluateServiceBlockingStub evaluateServiceClient;
 
     private final RestClient restClient;
+    private final ExecutorService executor;
 
-    public TestService(RestClient restClient) {
+    public TestService(RestClient restClient, ExecutorService executor) {
         this.restClient = restClient;
+        this.executor = executor;
     }
 
     public String testFlow() {
-        final Instant start = Instant.now();
-        // Step 1: Generate a random expression
-        String expression = generateExpression();
-        log.info("Generated expression: {}", expression);
+        try {
+            final Instant start = Instant.now();
+            // Step 1: Generate a random expression
+            String expression = this.executor.submit(this::generateExpression).get();
+            //dummy calls to check if virtual threads make it parallel
+            String dummy1 = this.executor.submit(this::generateExpression).get();
+            String dummy2 = this.executor.submit(this::generateExpression).get();
 
-        //dummy calls to check if virtual threads make it parallel
-        String dummy1 = generateExpression();
-        String dummy2 = generateExpression();
+            log.info("Generated expression: {}", expression);
 
+            // Step 2: Evaluate the expression via gRPC
+            // Step 2.1: create the request format
+            Evaluate.EvaluateRequest request = Evaluate.EvaluateRequest.newBuilder()
+                    .setExpression(expression)
+                    .build();
 
-        // Step 2: Evaluate the expression via gRPC
-        // Step 2.1: create the request format
-        Evaluate.EvaluateRequest request = Evaluate.EvaluateRequest.newBuilder()
-                .setExpression(expression)
-                .build();
+            // Step 2.2: Send request and evaluate the tokens
+            var result = evaluateServiceClient.evaluate(request).getResult();
 
-        // Step 2.2: Send request and evaluate the tokens
-        var result = evaluateServiceClient.evaluate(request).getResult();
-
-        final String resultString = String.format("generated {%s} and evaluated its value as {%.8f} in %d ms",
-                expression, result, Duration.between(start, Instant.now()).toMillis());
-        log.info(resultString);
-        return resultString;
+            final String resultString = String.format("generated {%s} and evaluated its value as {%.8f} in %d ms",
+                    expression, result, Duration.between(start, Instant.now()).toMillis());
+            log.info(resultString);
+            return resultString;
+        } catch (Exception e) {
+            log.error("error", e);
+            throw new RuntimeException(e);
+        }
     }
 
     private String generateExpression() {
