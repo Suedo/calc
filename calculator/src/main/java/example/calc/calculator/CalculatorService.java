530b2489434ff3fe5def25d11159251479b20ed5
Somjit Nag <somjitnag192@gmail.com>
2025-01-09 16:16:28 +0530

    propagating errors

--- calculator/src/main/java/example/calc/calculator/CalculatorService.java
+++ calculator/src/main/java/example/calc/calculator/CalculatorService.java
@@ -1,7 +1,9 @@
 package example.calc.calculator;
 
+import example.demo.shared.exceptions.DivisionByZeroException;
 import example.demo.shared.proto.CalculatorGrpc;
 import example.demo.shared.proto.CalculatorOuterClass;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -24,18 +26,29 @@ public class CalculatorService extends CalculatorGrpc.CalculatorImplBase {
 
         sleep(10);
 
-        double result = switch (operation) {
-            case ADD -> a + b;
-            case SUBTRACT -> a - b;
-            case MULTIPLY -> a * b;
-            case DIVIDE -> a / b;
-            default -> throw new IllegalArgumentException("Invalid operation");
-        };
+        try {
+            double result = switch (operation) {
+                case ADD -> a + b;
+                case SUBTRACT -> a - b;
+                case MULTIPLY -> a * b;
+                case DIVIDE -> {
+                    if (b == 0) throw new DivisionByZeroException("Division by zero is not allowed");
+                    yield a / b;
+                }
+                default -> throw new IllegalArgumentException("Invalid operation");
+            };
 
-        CalculatorOuterClass.CalculatorResponse response = CalculatorOuterClass.CalculatorResponse.newBuilder()
-                .setResult(result)
-                .build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
+
+            CalculatorOuterClass.CalculatorResponse response = CalculatorOuterClass.CalculatorResponse.newBuilder()
+                    .setResult(result)
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (DivisionByZeroException e) {
+            log.error("Division by zero error: {}", e.getMessage());
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asException());
+        }
     }
 }
